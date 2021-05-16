d16dabbac5dd6f53cbd9ebbaf2eb76371c4674ea
BillLin1209 <zzx123bill@gmail.com>
2021-05-16 14:35:32 +0800

    setup modules [ware, coupon, product, order, member ]

---
+++ shoppingmall_product/src/main/java/com/bill/shoppingmall/shoppingmall_product/ShoppingmallProductApplication.java
@@ -0,0 +1,13 @@
+package com.bill.shoppingmall.shoppingmall_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallProductApplication.class, args);
+    }
+
+}
---
+++ shoppingmall_product/src/test/java/com/bill/shoppingmall/shoppingmall_product/ShoppingmallProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.shoppingmall.shoppingmall_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
