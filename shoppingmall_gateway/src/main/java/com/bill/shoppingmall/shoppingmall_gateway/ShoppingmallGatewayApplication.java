69f84c6e2286fa11dd15bc0e7537f02a4568df69
BillLin1209 <zzx123bill@gmail.com>
2021-06-29 17:34:30 +0800

    add gateway module

---
+++ shoppingmall_gateway/src/main/java/com/bill/shoppingmall/shoppingmall_gateway/ShoppingmallGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bill.shoppingmall.shoppingmall_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingmallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingmallGatewayApplication.class, args);
+    }
+
+}
---
+++ shoppingmall_gateway/src/test/java/com/bill/shoppingmall/shoppingmall_gateway/ShoppingmallGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bill.shoppingmall.shoppingmall_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingmallGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
